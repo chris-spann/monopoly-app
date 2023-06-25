39695a6afb5942549e2b1e8dfada1339498d9f01
Chris Spann <cspanndev@gmail.com>
2023-06-24 21:05:29 -0500

    cleanup

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,6 +1,4 @@
 # Import all models here so alembic can discover them
-from models.author import Author  # noqa: F401
-from models.book import Book  # noqa: F401
 from models.card import Card  # noqa: F401
 from models.gamespace import GameSpace  # noqa: F401
 from models.player import Player  # noqa: F401
--- backend/routers/__init__.py
+++ backend/routers/__init__.py
@@ -3,8 +3,6 @@ from routers import cards, deeds, gamespaces, players, root
 
 api_router = APIRouter()
 
-# api_router.include_router(authors.router, tags=["authors"])
-# api_router.include_router(books.router, tags=["books"])
 api_router.include_router(cards.router, tags=["cards"])
 api_router.include_router(deeds.router, tags=["deeds"])
 api_router.include_router(gamespaces.router, tags=["gamespaces"])
